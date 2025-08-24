732cc4b852faa9ea2cd85466deefe4e339d1f93f
georgeerel <georgeerel1@gmail.com>
2025-08-24 19:58:07 +0300

    build ModuleConfig because raz gay

---
+++ src/main/java/frc/robot/ModuleConfig.java
@@ -0,0 +1,54 @@
+package frc.robot;
+
+import com.ctre.phoenix6.CANBus;
+
+import frc.Demacia.utils.Motors.SparkConfig;
+import frc.Demacia.utils.Motors.TalonConfig;
+import frc.Demacia.utils.Motors.BaseMotorConfig.Canbus;
+
+public class ModuleConfig {
+    public int ModuleID;
+    public SparkConfig SteerConfig;
+    public TalonConfig DriveConfig;
+    public int CanCoderID;
+    public String Name;
+    public int SteerID;
+    public int DriveID;
+    public double GearRatioSteer = 1 / (150.0 / 7.0);
+    public double GearRatioDrive = 1 / 6.75;
+    public double Diameter;
+    public double CanCoderOffset;
+
+    public ModuleConfig(int moduleID, int steerID, int driveID, int canCoderID, String name, double diameter, double canCoderOffset) {
+        this.ModuleID = moduleID;
+        this.SteerID = steerID;
+        this.DriveID = driveID;
+        this.CanCoderID = canCoderID;
+        this.Name = name;
+        this.Diameter = diameter;
+        this.CanCoderOffset = canCoderOffset;
+
+
+        SteerConfig = new SparkConfig(steerID, name + "/Steer")
+            .withBrake(true)
+            .withInvert(false)
+            .withDegreesMotor(GearRatioSteer)
+            .withCurrent(20)
+            .withVolts(8)
+            .withPID(0, 0, 0, 0, 0, 0, 0)
+            .withRampTime(0.2)
+            .withMotionParam(720, 1200, 2000)
+            .withMaxPositionError(0.5);
+
+        DriveConfig = new TalonConfig(driveID, Canbus.Rio, name + "/Drive")
+            .withBrake(true)
+            .withInvert(false)
+            .withMeterMotor(GearRatioDrive, diameter)
+            .withCurrent(20)
+            .withVolts(12)
+            .withPID(0, 0, 0, 0, 0, 0, 0)
+            .withRampTime(0.2)
+            .withMotionParam(720, 1200, 2000);
+
+    }
+}
