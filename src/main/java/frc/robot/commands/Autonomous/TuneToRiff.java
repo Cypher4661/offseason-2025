2df843305a6c64a55c26e6cc525e4bde2bd33458
georgeerel <georgeerel1@gmail.com>
2025-10-04 07:42:32 +0300

    change CanCoder off set but if it's worng it't because of RAZ

--- src/main/java/frc/robot/commands/Autonomous/TuneToRiff.java
+++ src/main/java/frc/robot/commands/Autonomous/TuneToRiff.java
@@ -7,7 +7,7 @@ import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Swerve.ChassisSubsystem;
 import frc.robot.subsystems.Swerve.SwerveConstants;
-//import frc.robot.subsystems.VisionSubsystem;\
+//import frc.robot.subsystems.VisionSubsystem;
 import frc.robot.Constants;
 
 public class TuneToRiff extends Command{
